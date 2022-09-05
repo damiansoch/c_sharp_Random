da3ae39d10f64a544726011112488b121f12c127
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-09-05 06:10:31 +0100

    Add project files.

---
+++ Random/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomExample
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            //dice
+            Random dice = new Random();
+            int numEyes;
+
+            for(int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1,7);
+                Console.Write(numEyes +" ");
+            }
+
+            //fortune teller
+            Console.WriteLine("\nAsk your question");
+            string question = Console.ReadLine();
+
+            string[] answers = new string[] { "Yes", "No", "Maybe", };
+
+            Random random = new Random();
+            string randomAnswer = answers[random.Next(0, 3)];
+
+            Console.WriteLine($"Your question was: {question}.\n");
+            Console.WriteLine($"The answer is: {randomAnswer}");
+            Console.ReadLine();
+        }
+        
+    }
+}
